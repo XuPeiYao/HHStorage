2fb314aba44e013640832d83124726d1dc24c58b
XuPeiYao <st89382000@gmail.com>
2017-12-17 22:47:54 +0800

    實作基礎控制器

--- HHStorage/Base/BaseController.cs
+++ HHStorage/Base/BaseController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
+using HHStorage.Models.API.Response;
 using HHStorage.Models.EF;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace HHStorage.Base {
     /// <summary>
@@ -17,11 +20,37 @@ namespace HHStorage.Base {
         public HHStorageContext Database { get; private set; }
 
         /// <summary>
+        /// 目前登入使用者
+        /// </summary>
+        public new User User {
+            get {
+                var userId = base.User.Claims.SingleOrDefault(x =>
+                    x.Properties.Any(y => y.Value == JwtRegisteredClaimNames.Sub)
+                )?.Value;
+
+                return Database.User.SingleOrDefault(x => x.Id == userId);
+            }
+        }
+
+        /// <summary>
         /// 控制器基底建構子
         /// </summary>
         /// <param name="database">資料庫內容</param>
         public BaseController(HHStorageContext database) {
             this.Database = database;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context) {
+            if (context.Exception != null) {
+                context.Result = new JsonResult(new APIResponse() {
+                    Result = new APIError() {
+                        Message = context.Exception.Message
+                    }
+                });
+                context.ExceptionHandled = true;
+            }
+
+            base.OnActionExecuted(context);
         }
     }
 }
