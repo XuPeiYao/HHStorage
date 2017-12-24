49f6c0f2b0603a79d3f108a4b8f1d208e94cba0c
XuPeiYao <st89382000@gmail.com>
2017-12-24 11:41:13 +0800

    加入認證失敗訊息處理

--- HHStorage/Startup.cs
+++ HHStorage/Startup.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using EzCoreKit.AspNetCore;
 using EzCoreKit.Cryptography;
+using EzCoreKit.MIME;
+using HHStorage.Exceptions;
 using HHStorage.Models.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -16,6 +19,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace HHStorage {
@@ -49,17 +54,16 @@ namespace HHStorage {
                     "Bearer",
                     new ApiKeyScheme() {
                         In = "header",
-                        Description = "Please insert JWT with Bearer into field",
+                        Description = "請輸入Bearer類型的JWT在此欄位",
                         Name = "Authorization",
-                        Type = "apiKey"
+                        Type = "apiKey",
                     });
                 void IncludeXmlComment(string filename) {
                     c.IncludeXmlComments(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, filename));
-
                 }
                 IncludeXmlComment("HHStorage.xml");
                 IncludeXmlComment("HHStorage.Models.API.xml");
-
+                IncludeXmlComment("HHStorage.Exceptions.xml");
             });
         }
 
@@ -70,6 +74,32 @@ namespace HHStorage {
             }
 
             app.UseAuthentication();
+
+            //錯誤處理
+            Action<IApplicationBuilder> errorHandler = (builder) => {
+                builder.Run(handler => {
+                    return Task.Run(() => {
+                        //取得狀態碼
+                        int code = handler.Response.StatusCode;
+
+                        //檢查是否存在指定的對應
+                        if (code == 401) {
+                            handler.Response.ContentType = DeclareMIME.JavaScript_Object_Notation_JSON;
+
+                            //寫出錯誤頁面內容
+                            var serializerSettings = new JsonSerializerSettings();
+                            serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                            byte[] Content = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new AuthorizeException(), serializerSettings));
+
+                            handler.Response.Body.WriteAsync(Content, 0, Content.Length);
+                            return;
+                        }
+                    });
+                });
+            };
+
+            //狀態對應
+            app.UseStatusCodePages(errorHandler);
 
             app.UseMvc();
 
